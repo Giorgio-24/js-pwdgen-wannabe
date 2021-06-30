7444b2484881965d2fdc20e4cc24efbcc4a1ed5c
Giorgio Andrei <cipuandrei@libero.it>
2021-06-30 16:01:38 +0200

    vars-added

--- script.js
+++ script.js
@@ -3,4 +3,9 @@
    Chiedi: colore preferito;
     Scrivi sulla pagine nomecognomecolore21 come password    */
 
-var userPassword = prompt('')
+
+
+var firstName = prompt("Qual'è il tuo nome?");
+var lastName = prompt("Qual'è il tuo cognome?");
+var favouriteColor = prompt("Qual'è il tuo colore preferito?");
+console.log(firstName, lastName, favouriteColorGiorgio)

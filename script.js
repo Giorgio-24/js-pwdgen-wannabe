1bdcabe46d4674656647b4f62988f12b0a6637c6
Giorgio Andrei <cipuandrei@libero.it>
2021-06-30 16:15:14 +0200

    password-successfully-generated

--- script.js
+++ script.js
@@ -4,8 +4,18 @@
     Scrivi sulla pagine nomecognomecolore21 come password    */
 
 
+var message = 'La tua password é: '
 
 var firstName = prompt("Qual'è il tuo nome?");
+console.log(firstName)
+
 var lastName = prompt("Qual'è il tuo cognome?");
+console.log (lastName)
+
 var favouriteColor = prompt("Qual'è il tuo colore preferito?");
-console.log(firstName, lastName, favouriteColorGiorgio)
+console.log(favouriteColor)
+
+var userPassword = document.getElementById('user-password');
+
+userPassword.innerHTML = message + firstName + lastName + favouriteColor + 21;
+
